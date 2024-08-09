9a69e5f352cca26002f9c86926d5a14d8d04f05f
Pouya Esmaili-Dokht <poyaesy@yahoo.com>
2024-08-09 15:50:53 +0330

    fist version of graviton3 curves.

---
+++ CPU/Actual-hardware/ARM/Graviton3/docs/processing.md
@@ -0,0 +1,7 @@
+Upon completion of performance counter sampling we proceed to processing stage.
+
+This pipeline consists of four stages:
+- Raw data parsing and calculation of derived metrics (bandwidth and latency).
+- Data filtering: Automatic and manual removal of outliers.
+- Data post-processing: Calculating averages, smoothening the data.
+- Data output: Visualizing via plots or saving as CSV or custom data formats.
---
+++ CPU/Actual-hardware/ARM/Graviton3/runner.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+################################################################################
+# Copyright (c) 2023, Valéria S. Girelli
+#                     Pouya Esmaili Dokht
+#                     Mariana Carmin
+#                     Petar Radojkovic
+#                     Paul Carpenter
+#                     Eduard Ayguade
+#                     Contact: valeria.soldera [at] bsc [dot] es
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of the copyright holder nor the names
+#       of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+################################################################################
+
+
+# spack load likwid
+# spack load lmbench
+# spack load numactl
+
+
+
+
+
+./create_runner_config.sh config/graviton3.toml 
+
+. ./config.sh
+
+
+
+PTRCHASE_ARRAY_SIZE=$((${PTRCHASE_ARRAY_SIZE}))
+
+
+###########################
+# Setting the working dir #
+###########################
+
+# Setting working dir, output dir, and compiling the benchmarks according to the
+# configuration file
+export ROOT_DIR="$(pwd)"
+export ATE="$(date +"%d-%m-%Y-%H_%M_%S")"
+# OUTPUT="${NAME}_${MEM_TYPE}_${MEM_FREQ}_${DATE}/"
+export OUTPUT_DIR="${ROOT_DIR}/measuring/"
+
+export OUTPUT_DIR_BW="${OUTPUT_DIR}/bw"
+export OUTPUT_DIR_LAT="${OUTPUT_DIR}/lat"
+export OUTPUT_DIR_LOG="${OUTPUT_DIR}/log"
+mkdir -p ${OUTPUT_DIR_BW}
+mkdir -p ${OUTPUT_DIR_LAT}
+mkdir -p ${OUTPUT_DIR_LOG}
+
+
+export STREAM_DIR="${ROOT_DIR}/src/stream_mpi/"
+export PTRCHASE_DIR="${ROOT_DIR}/src/ptr_chase/"
+
+echo "*********** Working dir set. Compiling Stream..."
+cd ${STREAM_DIR}
+
+# make clean && make
+echo "*********** Stream compiled successfully. Compiling Ptr_chase..."
+
+
+
+# clear output directories
+cd ${OUTPUT_DIR_LOG}
+# rm *
+# rm .*
+# cd ${OUTPUT_DIR_BW}
+# rm *
+# rm .*
+# cd ${OUTPUT_DIR_LAT}
+# rm *
+# rm .*
+# rm ../../output/*
+
+cd ${ROOT_DIR}
+cp ./config.sh ${OUTPUT_DIR}
+
+
+
+# set variables for bw measurment
+export STREAM_CORE_LIST=${STREAM_CORE_LIST} 
+export STREAM_NUMACTL_ADDITIONAL_ARGS=${STREAM_NUMACTL_ADDITIONAL_ARGS} 
+export STREAM_CORE_COUNT=${STREAM_CORE_COUNT} 
+export STREAM_DIR=${STREAM_DIR}
+export rd_percentage=${rd_percentage} 
+export pause=${pause} 
+export STREAM_LOG_FILE=${STREAM_LOG_FILE}
+export OUTPUT_DIR_BW=${OUTPUT_DIR_BW}
+export BW_TOOL=${BW_TOOL}
+export BW_TOOL_PATH=${BW_TOOL_PATH}
+export BW_TOOL_CORES=${BW_TOOL_CORES}
+export BW_TOOL_COUNTERS=${BW_TOOL_COUNTERS}
+export BW_TOOL_SAMPLE_TIME=${BW_TOOL_SAMPLE_TIME} 
+export TIME_STREAM_STABILIZE=${TIME_STREAM_STABILIZE}
+export TIME_AFTER_BW_MEAS=${TIME_AFTER_BW_MEAS}
+export BW_MEAS_REPS=${BW_MEAS_REPS}
+
+# set variables for latency measurment
+export PTRCHASE_NUM_INSTRUCTIONS=${PTRCHASE_NUM_INSTRUCTIONS}
+export PTRCHASE_NUM_ITERATIONS=${PTRCHASE_NUM_ITERATIONS}
+export PTRCHASE_ARRAY_SIZE=${PTRCHASE_ARRAY_SIZE}
+export CACHE_LINE=${CACHE_LINE}
+export LAT_MEAS_REPS=${LAT_MEAS_REPS}
+export LAT_TOOL=${LAT_TOOL}
+export PTRCHASE_CORE=${PTRCHASE_CORE}
+export LAT_TOOL_PATH=${LAT_TOOL_PATH}
+export LAT_TOOL_COUNTERS=${LAT_TOOL_COUNTERS}
+
+export POINT_REPS=${POINT_REPS}
+
+
+
+
+
+cd ${PTRCHASE_DIR}
+# # TODO check about the mpicc module or at least add it to the documentation as a required package
+# make clean && make
+
+echo "*********** Ptr_chase compiled successfully."
+
+
+
+
+cd ${ROOT_DIR}
+
+
+# for ((point_rep=1; point_rep<=${POINT_REPS}; point_rep+=1)); do
+#     for ((rd_percentage=RWRATIO_MIN; rd_percentage<=RWRATIO_MAX; rd_percentage+=RWRATIO_STEP)); do
+#         for pause in ${PAUSES}; do
+
+#                 export rd_percentage
+#                 export pause
+                
+#                 echo "*********** Iteration: rd_percentage=${rd_percentage} pause=${pause} rep=${point_rep}"
+
+#                 sbatch submit_main.job.bash
+
+#                 # to avoid submitting too much jobs to slurm
+#                 jobsWAitingInTheQueue=$(squeue | wc -l)
+#                 while [ $jobsWAitingInTheQueue -gt 25 ]
+#                 do
+#                     sleep 150
+#                     echo 'waiting for the free node...'
+#                     jobsWAitingInTheQueue=$(squeue | wc -l)
+#                 done
+
+#         done
+
+
+#     done
+# done
+
+
+
+
+
+
+
+
+
+
+
+
+###################
+# Post-processing #
+###################
+# echo "start processing data..."
+PROCESSING_DIR="${ROOT_DIR}/processing/"
+${PROCESSING_DIR}/main.py ${OUTPUT_DIR}
+
+# TODO something is not being killed when this process finishes, search for kill
+# any process started inside bash script
