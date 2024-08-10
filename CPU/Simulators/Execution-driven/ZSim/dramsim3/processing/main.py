481ad621cc8939f5113e2c1f9d24961ca97648bd
Pouya Esmaili-Dokht <poyaesy@yahoo.com>
2024-08-10 21:37:56 +0330

    add zsim with dramsim3 (zsim.h5 are eliminated due to storage limitation)

---
+++ CPU/Simulators/Execution-driven/ZSim/dramsim3/processing/main.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2024, Barcelona Supercomputing Center
+# Contact: pouya.esmaili    [at] bsc [dot] es
+#          petar.radojkovic [at] bsc [dot] es
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of the copyright holder nor the names
+#       of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import os
+import os.path
+import pandas as pd
+import numpy as np
+import toml
+import calculator
+import corrections
+import parse
+import utils
+import visualization
+
+def main():
+    argv = sys.argv
+    if len(argv) < 2:
+        print('usage ' + argv[0] + ' working_dir')
+        exit()
+
+    working_dir = os.path.abspath(argv[1])
+    output_dir = os.path.join(working_dir, 'output/')
+
+    with open(os.path.join(working_dir, 'config.sh')) as f:
+        config = dict(x.rstrip().split("=", 1) for x in f)
+    # print(config)
+
+    
+
+    # process BW
+    bw_parser = parse.Parser('bandwidth', os.path.join(working_dir, 'bw-lat'), config)    
+    df_bw_raw = bw_parser.process()
+    # ask valeria what is this doing...
+    visualization.metric_per_pause(df_bw_raw, 'bandwidth')
+    df_bw_mean = calculator.MeanCalculator.calculate(df_bw_raw, 'bandwidth')
+
+
+    # process LAT
+    lat_parser = parse.Parser('latency', os.path.join(working_dir, 'bw-lat'), config)
+    df_lat_raw = lat_parser.process()
+    visualization.metric_per_pause(df_lat_raw, 'latency')
+    df_lat_mean = calculator.MeanCalculator.calculate(df_lat_raw, 'latency')
+
+
+    
+
+    # merge BW and LAT dataframes
+    df = df_bw_mean.merge(df_lat_mean)
+
+ 
+
+    # The dataframe is split by read-write ratio to facilitate furher processing steps
+    # The map translates rd_percentage to the corresponding dataframe (part of the original dataframe)
+    dfs_rw = utils.split_dataframe_by_rd_percentage(df)
+
+    # quick fix of something i dont understand yet
+    # df = dfs_rw
+    # Remove outliers
+    # TODO (an important TODO...)
+
+    # Smoothen the curves
+    dfs_rw = corrections.smooth_curves(config, dfs_rw)
+    
+
+
+    #  processing data for saturate bw range and max latency range 
+    tmpMin = []
+    for df_rw in dfs_rw:
+        tmpMin.append(dfs_rw[df_rw]['latency_smooth'].min()) 
+    leadOffLatency= min(tmpMin)
+    latencyThreshhold = leadOffLatency*2
+    bandwidthMin = []
+    for df_rw in dfs_rw:
+        mylist = (dfs_rw[df_rw]['latency_smooth'],dfs_rw[df_rw]['bandwidth_smooth'])
+        # print(mylist)
+        tmplist = [x for x in mylist[0] if x>latencyThreshhold]
+        # print(tmplist)
+        if tmplist == []:
+            continue
+        myIndex = find(mylist[0], tmplist[-1]) 
+        bandwidthMin.append(mylist[1][myIndex])
+
+    max_bw = int(config['MEM_MAX_CHANNELS'])*8*int(config['MEM_FREQ']) / 1000 #in GB/s
+    print('min saturate bw', min(bandwidthMin), ' ' , min(bandwidthMin)/max_bw)
+    tmpMax = []
+    for df_rw in dfs_rw:
+        tmpMax.append(dfs_rw[df_rw]['latency_smooth'].max()) 
+
+    print('max max latency is ', max(tmpMax))
+    print('max min latency is ', min(tmpMax))
+    # end of processing data for saturate bw range and max latency rang
+
+    # Save curves into files
+    # utils.save_curves_to_file(config, os.path.join(output_dir, 'curves'), dfs_rw)
+
+    # Merge into the final dataframe
+    # df = utils.merge_dataframes(list(dfs_rw.values()))
+
+    # save the final dataframe
+    # df.to_csv(os.path.join(output_dir, 'bandwidth-latency.csv'), index=False)
+
+
+    visualization.final_curves(config, os.path.join(output_dir, 'bandwidth-latency.pdf'), dfs_rw, lat_column='latency_smooth', bw_column='bandwidth_smooth', lat_unit='ns')
+
+    # visualization.final_curves(config, os.path.join(output_dir, 'bandwidth-latency.pdf'), dfs_rw, lat_column='latency_mean', bw_column='bandwidth_mean', lat_unit='ns')
+
+def find(s, el):
+    for i in s.index:
+        if s[i] == el: 
+            return i
+    return None
+
+if __name__ == '__main__':
+    main()
+
---
+++ CPU/Simulators/Execution-driven/ZSim/dramsim3/processing/utils.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2024, Barcelona Supercomputing Center
+# Contact: pouya.esmaili    [at] bsc [dot] es
+#          petar.radojkovic [at] bsc [dot] es
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of the copyright holder nor the names
+#       of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import pandas as pd
+
+def split_dataframe_by_rd_percentage(df):
+    """ Returns a map of read-write ratio -> corresponding sub-dataframe
+
+        The function preserves the columns from input dataframe
+
+        Parameters:
+        df (pandas DataFrame) - input dataframe
+
+        Returns:
+        dictionary - mapping read-write ratio to corresponding sub-DataFrame
+
+    """
+    rws = pd.unique(df['rd_percentage'])
+    df_rws = {}
+    for rd_percentage in rws:
+        df_rws[rd_percentage] = df[df['rd_percentage'] == rd_percentage].reset_index(drop=True)
+    return df_rws
+
+def merge_dataframes(dfs):
+    """ Concatenates a list of dataframes, assuming that all dataframes have the same columns
+
+        Parameters:
+        dfs (list) - List of input dataframes
+
+        Returns:
+        pandas DataFrame - resulting DataFrame
+    """
+    return pd.concat(dfs)
+
+def save_curves_to_file(config, outdir, dfs_rw, bw_column = 'bandwidth_smooth', lat_column = 'latency_smooth', lat_unit='ns'):
+    """ Saves bandwidth-latency curves in a provided folder in a format expected by Profet tool
+
+        Bandwidth is saved in MB/s and latency in cycles. The function expects bandwidth and latency in the input to be
+        in GB/s and nanoseconds, respectively.
+
+        Parameters:
+        config (dictionary) - configuration
+        outdir (string) - output folder where the files are written (the folder is created if it does not exist)
+        dfs_rw (dictionary) - maps read-write ratio to correpsonding dataframe
+        bw_column (string) - column used for bandwidth in provided dataframe (default: bandwidth_smooth)
+        lat_column (string) - column used for latency in provided dataframe (default: latency_smooth)
+        lat_unit (string) - unit for provided latency. Possible options 'ns', 'cycles' (default: 'ns')
+
+        Returns:
+        nothing
+
+    """
+    assert(lat_unit in ['ns', 'cycles'])
+    os.makedirs(outdir, exist_ok=True)
+    for rw, df in dfs_rw.items():
+        df_copy = df[[bw_column, lat_column]].copy()
+
+        # Convert bandwidth from GB/s to MB/s
+        df_copy.loc[:, bw_column] = df_copy[bw_column].apply(lambda bw : bw * 1000)
+        
+        # Convert latency to cycles if necessary
+        if lat_unit == 'ns':
+            df_copy.loc[:, lat_column] = df_copy[lat_column].apply(lambda lat : lat * float(config['CPU_FREQ']))
+
+        df_copy.to_csv(os.path.join(outdir, f'bwlat_{rw}.txt'), sep='\t', index=False, header=False)
